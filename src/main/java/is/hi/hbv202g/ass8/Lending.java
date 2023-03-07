eef1821dfa1a706b35b903b096c9a5b73d3e95d0
manifreyr <manifreyrr@gmail.com>
2023-03-07 10:41:20 +0000

    Lending

--- src/main/java/is/hi/hbv202g/ass8/Lending.java
+++ src/main/java/is/hi/hbv202g/ass8/Lending.java
@@ -1,6 +1,39 @@
 package is.hi.hbv202g.ass8;
 
-public class Lending {
-    private Book book;
+import java.time.LocalDate;
 
+public class Lending {
+
+    private Book book;
+    private User user;
+
+    private LocalDate dueDate;
+    public Lending(Book book, User user) {
+        this.book = book;
+        this.user = user;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
